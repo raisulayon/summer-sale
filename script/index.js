f2b8839204fa70cfb013a82a8137d627e2d2a15c
ycmcayon <ycmc.ayon@gmail.com>
2023-08-22 01:51:40 +0600

    new commit

--- script/index.js
+++ script/index.js
@@ -1,44 +1,17 @@
-function getElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const elementValue = parseFloat(elementValueString);
-    return elementValue;
-}   
+let total =0;
 
-document.getElementById('product1').addEventListener('click', function(){
-    const price = getElementValueById('prod1');
-})
-document.getElementById('product2').addEventListener('click', function(){
-    const price = getElementValueById('prod2');
-})
-document.getElementById('product3').addEventListener('click', function(){
-    const price = getElementValueById('prod3');
-})
-document.getElementById('product4').addEventListener('click', function(){
-    const price = getElementValueById('prod4');
-})
-document.getElementById('product5').addEventListener('click', function(){
-    const price = getElementValueById('prod5');
-})
-document.getElementById('product6').addEventListener('click', function(){
-    const price = getElementValueById('prod6');
-})
-document.getElementById('product7').addEventListener('click', function(){
-    const price = getElementValueById('prod7');
-})
-document.getElementById('product8').addEventListener('click', function(){
-    const price = getElementValueById('prod8');
-})
-document.getElementById('product9').addEventListener('click', function(){
-    const price = getElementValueById('prod9');
-})
+function handleClickBtn(target){
+    // console.log(target.parentNode.childNodes[3].innerText);
+const selectedProducts = document.getElementById('calculation-entry');
+const productName = target.parentNode.childNodes[3].innerText;
+const li = document.createElement('li');
+li.innerText = productName;
+selectedProducts.appendChild(li);
 
-function  addToCalculationEntry(elementType, price){
-    const calculationEntry = document.getElementById('calculation-entry');
-    const p = document.createElement('p');
-    const count = calculationEntry.childElementCount;
-    p.innerHTML =  `${count +1}. ${product} ${price} `
-    calculationEntry.appendChild(p);
+const price = target.parentNode.childNodes[5].innerText.split(" ")[0];
+total = parseInt(total) + parseInt(price);
+document.getElementById('total-price').innerText=total;
+
+
 }
-
 
